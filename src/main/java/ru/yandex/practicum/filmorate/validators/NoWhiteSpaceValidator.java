4294165668b594a0ab2f046ece03dd468654bd63
owl-t <d4vydovoliver@yandex.ru>
2023-12-05 01:53:07 +0700

    fix: code style

--- src/main/java/ru/yandex/practicum/filmorate/validators/NoWhiteSpaceValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/NoWhiteSpaceValidator.java
@@ -6,7 +6,6 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class NoWhiteSpaceValidator implements ConstraintValidator<NoWhiteSpace, String> {
-
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         return !value.contains(" ");
