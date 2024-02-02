e5c3fe4b36f1c874a279853afff6fed52d23d8ba
John_SL <vansol9797@yandex.ru>
2024-02-02 09:27:22 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/entity/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/entity/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
